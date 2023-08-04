8bab5171f80fa247c95298a1ad9a4ae2af07d420
folorad <fduang@gmail.com>
2023-08-04 14:13:29 +0100

    add 'new card' button with styling and logic

--- index.js
+++ index.js
@@ -3,15 +3,21 @@
 // 2. Create a variable, sum and set it to the sum of the two cards
 
 let firstCard = 10;
-let secondCard = 11;
+let secondCard = 4;
 let sum = firstCard + secondCard;
 let hasBlackJack = false; // keeps track state of the game
 let isAlive = true; // is the player still in the game
 let message = "";
 let messageEl = document.getElementById("message-el");
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
 //console.log(messageEl)
 
 function startGame() {
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+
+
 // 3. conditional logic for game rules.
     if (sum <= 20){
         message = "Do you want to draw a new card? "
@@ -25,6 +31,12 @@ function startGame() {
     messageEl.textContent = message
 }
 
+function newCard(){
+    console.log('Drawing a new card from the deck!')
+    let card = 7 
+    sum += card
+    startGame()
+}
 
 
 // CASH OUT
