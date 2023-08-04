890ebd4a62a7095ffd3262512d1f662c8e17d10d
folorad <fduang@gmail.com>
2023-08-04 13:09:05 +0100

    add style and startGame function to start game button

--- index.js
+++ index.js
@@ -3,27 +3,27 @@
 // 2. Create a variable, sum and set it to the sum of the two cards
 
 let firstCard = 10;
-let secondCard = 12;
+let secondCard = 11;
 let sum = firstCard + secondCard;
 let hasBlackJack = false; // keeps track state of the game
 let isAlive = true; // is the player still in the game
 let message = "";
 
-
-//console.log(sum)
+function startGame() {
 // 3. conditional logic for game rules.
-if (sum <= 20){
-    message = "Do you want to draw a new card? "
-} else if (sum === 21) {
-    message = "Wohoo! You've got Black Jack! "
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! "
-    isAlive = false
-  
+    if (sum <= 20){
+        message = "Do you want to draw a new card? "
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Black Jack! "
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! "
+        isAlive = false
+    }
+    console.log(message)
 }
+
 
 
 // CASH OUT
 
-console.log(message)
